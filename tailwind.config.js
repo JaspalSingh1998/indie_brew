627d3597c7882e93337f99955d56109b3bf17838
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-08 18:07:28 +0530

    Project setup with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      DM: ["DM Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
