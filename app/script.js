ec1ac58fb2afc654101506ce203483b073ddeddf
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-09 13:48:48 +0530

    Finished Project

---
+++ app/script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+toggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
